e7454eac6b5f714d6ab1dfe6ce5546f19b2fa3cf
Parker Selbert <parker@sorentwo.com>
2020-03-03 09:28:31 -0600

    Cache dialyzer and run CI for pull requests

--- mix.exs
+++ mix.exs
@@ -27,6 +27,7 @@ defmodule Oban.MixProject do
       # Dialyzer
       dialyzer: [
         plt_add_apps: [:ex_unit],
+        plt_core_path: "_build/#{Mix.env()}",
         flags: [:error_handling, :race_conditions, :underspecs]
       ],
 
