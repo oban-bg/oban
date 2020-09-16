5b36295c47e90b66416e80b9c8efddef7d896f52
Parker Selbert <parker@sorentwo.com>
2020-09-16 08:37:53 -0500

    Lightly reformat registry functions for pipelines

--- lib/oban/registry.ex
+++ lib/oban/registry.ex
@@ -6,20 +6,24 @@ defmodule Oban.Registry do
   @type value :: term()
 
   def child_spec(_arg) do
-    Supervisor.child_spec(
-      Registry.child_spec(keys: :unique, name: __MODULE__),
-      id: __MODULE__
-    )
+    [keys: :unique, name: __MODULE__]
+    |> Registry.child_spec()
+    |> Supervisor.child_spec(id: __MODULE__)
   end
 
   @spec config(Oban.name()) :: Oban.Config.t()
   def config(oban_name) do
     [{_pid, config}] = Registry.lookup(__MODULE__, oban_name)
+
     config
   end
 
   @spec whereis(Oban.name(), role()) :: pid() | nil
-  def whereis(oban_name, role \\ nil), do: GenServer.whereis(via(oban_name, role))
+  def whereis(oban_name, role \\ nil) do
+    oban_name
+    |> via(role)
+    |> GenServer.whereis()
+  end
 
   @spec via(Oban.name(), role(), value()) :: {:via, Registry, {__MODULE__, key()}}
   def via(oban_name, role \\ nil, value \\ nil)
