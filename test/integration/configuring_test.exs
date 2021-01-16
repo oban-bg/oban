c7f8f95724be01444afdee13a2f30af0b47e692c
Parker Selbert <parker@sorentwo.com>
2021-01-16 11:28:35 -0600

    Check queue states for configuring test

--- test/integration/configuring_test.exs
+++ test/integration/configuring_test.exs
@@ -4,32 +4,20 @@ defmodule Oban.Integration.ConfiguringTest do
   @moduletag :integration
 
   test "configuring queue limits and poll intervals" do
-    start_supervised_oban!(
-      dispatch_cooldown: 1,
-      queues: [
-        alpha: 1,
-        gamma: [limit: 1, dispatch_cooldown: 5],
-        omega: [limit: 1, paused: true]
-      ]
-    )
+    name =
+      start_supervised_oban!(
+        dispatch_cooldown: 1,
+        queues: [
+          alpha: 1,
+          gamma: [limit: 1, dispatch_cooldown: 5],
+          omega: [limit: 1, paused: true]
+        ]
+      )
 
-    # Alpha has limit 1, only one of these will start
-    insert!([ref: 1, sleep: 1_000], queue: :alpha)
-    insert!([ref: 2, sleep: 1_000], queue: :alpha)
-
-    assert_receive {:started, 1}
-    refute_receive {:started, 2}
-
-    # Gamma has limit 1, all only two of these will start
-    insert!([ref: 3, sleep: 1_000], queue: :gamma)
-    insert!([ref: 4, sleep: 1_000], queue: :gamma)
-
-    assert_receive {:started, 3}
-    refute_receive {:started, 4}
-
-    # Omega is paused, this won't start
-    insert!([ref: 5, sleep: 5], queue: :omega)
-
-    refute_receive {:started, 5}
+    with_backoff(fn ->
+      assert %{limit: 1} = Oban.check_queue(name, queue: :alpha)
+      assert %{limit: 1} = Oban.check_queue(name, queue: :gamma)
+      assert %{limit: 1, paused: true} = Oban.check_queue(name, queue: :omega)
+    end)
   end
 end
