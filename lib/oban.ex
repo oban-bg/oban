c173cac8a077f4528710cf420281b299365a357d
Austin Ziegler <austin@zieglers.ca>
2021-02-07 23:17:58 -0500

    Address review comments
    - Remove CHANGELOG modifications
    - Extract type references to `Changeset.t(Job.t)` as `Job.changeset()`;
      add a `Job.changeset_list()` type, as well as moving the previously
      defined changeset wrapper function type to `Job` as
      `Job.changeset_list_fun()`; created a `Job.changeset_fun()` type to
      work with the other Multi-support function.
    - Updated external functions to refer to the right types.

--- lib/oban.ex
+++ lib/oban.ex
@@ -54,7 +54,7 @@ defmodule Oban do
 
   @type drain_result :: %{failure: non_neg_integer(), success: non_neg_integer()}
 
-  @type job_changeset :: Changeset.t(Job.t())
+  @type changesets_or_wrapper :: Job.changeset_list() | %{changesets: Job.changeset_list()}
 
   @version Mix.Project.config()[:version]
 
@@ -236,8 +236,8 @@ defmodule Oban do
       {:ok, job} = Oban.insert(MyApp.Worker.new(%{id: 1}, unique: [period: 30]))
   """
   @doc since: "0.7.0"
-  @spec insert(name(), job_changeset()) ::
-          {:ok, Job.t()} | {:error, job_changeset()} | {:error, term()}
+  @spec insert(name(), Job.changeset()) ::
+          {:ok, Job.t()} | {:error, Job.changeset()} | {:error, term()}
   def insert(name \\ __MODULE__, %Changeset{} = changeset) do
     name
     |> config()
@@ -264,7 +264,7 @@ defmodule Oban do
           name,
           multi :: Multi.t(),
           multi_name :: Multi.name(),
-          changeset_or_fun :: job_changeset() | fun()
+          changeset_or_fun :: Job.changeset() | Job.changeset_fun()
         ) :: Multi.t()
   def insert(name \\ __MODULE__, multi, multi_name, changeset_or_fun)
 
@@ -288,7 +288,7 @@ defmodule Oban do
       job = Oban.insert!(MyApp.Worker.new(%{id: 1}))
   """
   @doc since: "0.7.0"
-  @spec insert!(name(), job_changeset()) :: Job.t()
+  @spec insert!(name(), Job.changeset()) :: Job.t()
   def insert!(name \\ __MODULE__, %Changeset{} = changeset) do
     case insert(name, changeset) do
       {:ok, job} ->
@@ -320,10 +320,7 @@ defmodule Oban do
       |> Oban.insert_all()
   """
   @doc since: "0.9.0"
-  @spec insert_all(
-          name(),
-          changesets_or_wrapper :: [job_changeset()] | %{changesets: [job_changeset()]}
-        ) :: [Job.t()]
+  @spec insert_all(name(), changesets_or_wrapper()) :: [Job.t()]
   def insert_all(name \\ __MODULE__, changesets_or_wrapper)
 
   def insert_all(name, %{changesets: [_ | _] = changesets}) do
@@ -354,8 +351,7 @@ defmodule Oban do
           name(),
           multi :: Multi.t(),
           multi_name :: Multi.name(),
-          changesets_or_wrapper ::
-            [job_changeset()] | %{changesets: [job_changeset()]} | (map -> [job_changeset()])
+          changesets_or_wrapper() | Job.changeset_list_fun()
         ) :: Multi.t()
   def insert_all(name \\ __MODULE__, multi, multi_name, changesets_or_wrapper)
 
