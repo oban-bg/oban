19c79328bfe87232d0fd7e90362a5d3129be0cd0
Benjamin Milde <benni@kobrakai.de>
2021-03-08 15:49:45 +0100

    Make credo happy

--- lib/oban/plugins/cron.ex
+++ lib/oban/plugins/cron.ex
@@ -91,7 +91,7 @@ defmodule Oban.Plugins.Cron do
     |> Time.add(60)
     |> Map.put(:second, 0)
     |> Time.diff(time)
-    |> Integer.mod(86400)
+    |> Integer.mod(86_400)
     |> :timer.seconds()
   end
 
