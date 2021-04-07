0aa8ed4dd83df3da8820d4dd6af14edac6afd7fd
Parker Selbert <parker@sorentwo.com>
2021-04-07 17:50:19 -0500

    Remove the web troubleshooting guide
    The install instructions for Heroku and Gigalixir are now rolled into
    the Pro install guide.

--- mix.exs
+++ mix.exs
@@ -108,7 +108,6 @@ defmodule Oban.MixProject do
         "../oban_web/guides/web/installation.md": [filename: "web_installation"],
         "../oban_web/guides/web/customizing.md": [filename: "web_customizing"],
         "../oban_web/guides/web/telemetry.md": [filename: "web_telemetry"],
-        "../oban_web/guides/web/troubleshooting.md": [filename: "web_troubleshooting"],
         "../oban_web/CHANGELOG.md": [filename: "web-changelog", title: "Changelog"]
       ]
     else
