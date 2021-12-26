643310276ef7f039eaa341a9f5275c27e2c4f3f9
Parker Selbert <parker@sorentwo.com>
2021-12-25 18:04:22 -0600

    Migrate notifier to connection callback module
    An in-progress notifications update allows injecting a callback module
    into `Postgrex.Notifications`. The callback can handle listeners and
    notifications however the application requires, and more importantly,
    it allows running arbitrary queries.
    
    By switching to our own notification implementation we get control over
    relaying messgaes to subscribers (rather than proxying them all through
    the connection) and keep a persistent connection for leadership
    elections.

---
+++ lib/oban/connection.ex
@@ -0,0 +1,195 @@
+defmodule Oban.Connection do
+  @moduledoc false
+
+  @behaviour Postgrex.Notifications
+
+  alias Oban.{Config, Repo}
+  alias Postgrex.Notifications
+
+  # NOTE: Do we need circuit breaker stuff here?
+
+  defmodule State do
+    @moduledoc false
+
+    @enforce_keys [:conf]
+    defstruct [:conf, :from, :ref, channels: %{}, listeners: %{}]
+  end
+
+  @doc false
+  @spec child_spec(Keyword.t()) :: Supervisor.child_spec()
+  def child_spec(opts) do
+    conf = Keyword.fetch!(opts, :conf)
+    name = Keyword.get(opts, :name, __MODULE__)
+
+    call_opts = [conf: conf]
+
+    conn_opts =
+      conf
+      |> Repo.config()
+      |> Keyword.put(:name, name)
+
+    %{id: __MODULE__, start: {Notifications, :start_link, [__MODULE__, call_opts, conn_opts]}}
+  end
+
+  @impl Notifications
+  def init(args) do
+    {:ok, struct!(State, args)}
+  end
+
+  # TODO: Handle reconnection here later
+  @impl Notifications
+  def connect(state) do
+    {:noreply, state}
+  end
+
+  @impl Notifications
+  def handle_call({:query, query}, from, %State{} = state) do
+    {:query, query, %{state | from: from}}
+  end
+
+  def handle_call({:listen, channels}, {pid, _}, %State{} = state) do
+    new_channels = channels -- Map.keys(state.channels)
+
+    state =
+      state
+      |> put_listener(pid, channels)
+      |> put_channels(pid, channels)
+
+    if Enum.any?(new_channels) do
+      parts = Enum.map_join(new_channels, "\n", &~s(LISTEN "#{&1}";))
+      query = "DO $$BEGIN #{parts} END$$"
+
+      {:query, query, state}
+    else
+      {:reply, :ok, state}
+    end
+  end
+
+  def handle_call({:unlisten, channels}, {pid, _}, %State{} = state) do
+    state =
+      state
+      |> del_listener(pid, channels)
+      |> del_channels(pid, channels)
+
+    del_channels = Map.keys(state.channels) -- channels
+
+    if Enum.any?(del_channels) do
+      parts = Enum.map_join(del_channels, "\n", &~s(UNLISTEN "#{&1}";))
+      query = "DO $$BEGIN #{parts} END$$"
+
+      {:query, query, state}
+    else
+      {:reply, :ok, state}
+    end
+  end
+
+  @impl Notifications
+  def handle_info({:DOWN, _ref, :process, pid, _reason}, %State{} = state) do
+    # TODO: Clean up when handle_info works
+
+    {:noreply, state}
+  end
+
+  def handle_info(_message, state) do
+    {:noreply, state}
+  end
+
+  @impl Notifications
+  def handle_result(%{num_rows: 0}, state) do
+    {:noreply, state}
+  end
+
+  def handle_result(result, %State{from: from} = state) do
+    Notifications.reply(from, {:ok, result})
+
+    {:noreply, %{state | from: nil}}
+  end
+
+  @impl Notifications
+  def handle_notification(full_channel, payload, %State{} = state) do
+    decoded = Jason.decode!(payload)
+
+    if in_scope?(decoded, state.conf) do
+      channel = reverse_channel(full_channel)
+
+      for {pid, {_ref, channels}} <- state.listeners, full_channel in channels do
+        send(pid, {:notification, channel, decoded})
+      end
+    end
+
+    {:noreply, state}
+  end
+
+  # Helpers
+
+  defp reverse_channel(full_channel) do
+    [_prefix, "oban_" <> shortcut] = String.split(full_channel, ".", parts: 2)
+
+    String.to_existing_atom(shortcut)
+  end
+
+  defp in_scope?(%{"ident" => "any"}, _conf), do: true
+  defp in_scope?(%{"ident" => ident}, conf), do: Config.match_ident?(conf, ident)
+  defp in_scope?(_decoded, _conf), do: true
+
+  defp put_listener(%{listeners: listeners} = state, pid, channels) do
+    new_set = MapSet.new(channels)
+
+    listeners =
+      case Map.get(listeners, pid) do
+        {ref, old_set} ->
+          Map.replace!(listeners, pid, {ref, MapSet.union(old_set, new_set)})
+
+        nil ->
+          ref = Process.monitor(pid)
+
+          Map.put(listeners, pid, {ref, new_set})
+      end
+
+    %{state | listeners: listeners}
+  end
+
+  defp put_channels(state, pid, channels) do
+    listener_channels =
+      for channel <- channels, reduce: state.channels do
+        acc -> Map.update(acc, channel, [pid], &[pid | &1])
+      end
+
+    %{state | channels: listener_channels}
+  end
+
+  defp del_listener(%{listeners: listeners} = state, pid, channels) do
+    new_set = MapSet.new(channels)
+
+    listeners =
+      case Map.get(listeners, pid) do
+        {ref, old_set} ->
+          del_set = MapSet.difference(old_set, new_set)
+
+          if MapSet.size(del_set) == 0 do
+            Process.demonitor(ref)
+
+            Map.delete(listeners, pid)
+          else
+            Map.replace!(listeners, pid, {ref, del_set})
+          end
+
+        nil ->
+          listeners
+      end
+
+    %{state | listeners: listeners}
+  end
+
+  defp del_channels(state, pid, channels) do
+    listener_channels =
+      for channel <- channels, reduce: state.channels do
+        acc ->
+          Map.update(acc, channel, [], &List.delete(&1, pid))
+
+          if Enum.empty?(acc[channel]), do: Map.delete(acc, channel), else: acc
+      end
+
+    %{state | channels: listener_channels}
+  end
+end
--- lib/oban/postgres_notifier.ex
+++ lib/oban/postgres_notifier.ex
@@ -6,9 +6,10 @@ defmodule Oban.PostgresNotifier do
 
   The notifications system is built on PostgreSQL's `LISTEN/NOTIFY` functionality. Notifications
   are only delivered **after a transaction completes** and are de-duplicated before publishing.
-  Most applications run Ecto in sandbox mode while testing. Sandbox mode wraps each test in a
-  separate transaction which is rolled back after the test completes. That means the transaction
-  is never committed, which prevents delivering any notifications.
+  Typically, applications run Ecto in sandbox mode while testing, but sandbox mode wraps each test
+  in a separate transaction that's rolled back after the test completes. That means the
+  transaction is never committed, which prevents delivering any notifications.
+
   To test using notifications you must run Ecto without sandbox mode enabled.
   """
 
@@ -16,10 +17,7 @@ defmodule Oban.PostgresNotifier do
 
   use GenServer
 
-  import Oban.Breaker, only: [open_circuit: 1, trip_circuit: 3]
-
-  alias Oban.{Config, Repo}
-  alias Postgrex.Notifications
+  alias Oban.{Config, Connection, Registry, Repo}
 
   @mappings %{
     gossip: "oban_gossip",
@@ -31,42 +29,43 @@ defmodule Oban.PostgresNotifier do
     @moduledoc false
 
     @enforce_keys [:conf]
-    defstruct [
-      :conf,
-      :conn,
-      :name,
-      circuit: :enabled,
-      reset_timer: nil,
-      listeners: %{}
-    ]
+    defstruct [:conf]
   end
 
   @impl Oban.Notifier
   def start_link(opts) do
-    name = Keyword.get(opts, :name, __MODULE__)
+    {name, opts} = Keyword.pop(opts, :name, __MODULE__)
 
-    GenServer.start_link(__MODULE__, Map.new(opts), name: name)
+    GenServer.start_link(__MODULE__, opts, name: name)
   end
 
   @impl Oban.Notifier
   def listen(server, channels) do
-    GenServer.call(server, {:listen, channels})
+    with %State{conf: conf} <- GenServer.call(server, :get_state) do
+      conf.name
+      |> Registry.via(Connection)
+      |> GenServer.call({:listen, to_full_channels(conf, channels)})
+    end
   end
 
   @impl Oban.Notifier
   def unlisten(server, channels) do
-    GenServer.call(server, {:unlisten, channels})
+    with %State{conf: conf} <- GenServer.call(server, :get_state) do
+      conf.name
+      |> Registry.via(Connection)
+      |> GenServer.call({:unlisten, to_full_channels(conf, channels)})
+    end
   end
 
   @impl Oban.Notifier
   def notify(server, channel, payload) do
     with %State{conf: conf} <- GenServer.call(server, :get_state) do
-      channel = "#{conf.prefix}.#{@mappings[channel]}"
+      full_channel = Map.fetch!(@mappings, channel)
 
       Repo.query(
         conf,
         "SELECT pg_notify($1, payload) FROM json_array_elements_text($2::json) AS payload",
-        [channel, payload]
+        ["#{conf.prefix}.#{full_channel}", payload]
       )
 
       :ok
@@ -75,117 +74,20 @@ defmodule Oban.PostgresNotifier do
 
   @impl GenServer
   def init(opts) do
-    Process.flag(:trap_exit, true)
-
-    {:ok, struct!(State, opts), {:continue, :start}}
+    {:ok, struct!(State, opts)}
   end
 
   @impl GenServer
-  def handle_continue(:start, state) do
-    {:noreply, connect_and_listen(state)}
+  def handle_call(:get_state, _from, %State{} = state) do
+    {:reply, state, state}
   end
-
-  @impl GenServer
-  def handle_info({:DOWN, _ref, :process, pid, _reason}, %State{listeners: listeners} = state) do
-    {:noreply, %{state | listeners: Map.delete(listeners, pid)}}
-  end
-
-  def handle_info({:notification, _, _, prefixed_channel, payload}, state) do
-    full_channel =
-      prefixed_channel
-      |> String.split(".")
-      |> List.last()
-
-    channel = reverse_channel(full_channel)
-    decoded = if any_listeners?(state.listeners, full_channel), do: Jason.decode!(payload)
-
-    if in_scope?(decoded, state.conf) do
-      for {pid, channels} <- state.listeners, full_channel in channels do
-        send(pid, {:notification, channel, decoded})
-      end
-    end
-
-    {:noreply, state}
-  end
-
-  def handle_info({:EXIT, _pid, error}, %State{} = state) do
-    state = trip_circuit(error, [], state)
-
-    {:noreply, %{state | conn: nil}}
-  end
-
-  def handle_info(:reset_circuit, %State{circuit: :disabled} = state) do
-    state =
-      state
-      |> open_circuit()
-      |> connect_and_listen()
-
-    {:noreply, state}
-  end
-
-  def handle_info(_message, state) do
-    {:noreply, state}
-  end
-
-  @impl GenServer
-  def handle_call({:listen, channels}, {pid, _}, %State{listeners: listeners} = state) do
-    if Map.has_key?(listeners, pid) do
-      {:reply, :ok, state}
-    else
-      Process.monitor(pid)
-
-      full_channels = to_full_channels(channels)
-
-      {:reply, :ok, %{state | listeners: Map.put(listeners, pid, full_channels)}}
-    end
-  end
-
-  def handle_call({:unlisten, channels}, {pid, _}, %State{listeners: listeners} = state) do
-    orig_channels = Map.get(listeners, pid, [])
-
-    listeners =
-      case orig_channels -- to_full_channels(channels) do
-        [] -> Map.delete(listeners, pid)
-        new_channels -> Map.put(listeners, pid, new_channels)
-      end
-
-    {:reply, :ok, %{state | listeners: listeners}}
-  end
-
-  def handle_call(:get_state, _, state), do: {:reply, state, state}
 
   # Helpers
 
-  for {atom, name} <- @mappings do
-    defp reverse_channel(unquote(name)), do: unquote(atom)
-  end
-
-  defp to_full_channels(channels) do
+  defp to_full_channels(%Config{prefix: prefix}, channels) do
     @mappings
     |> Map.take(channels)
     |> Map.values()
+    |> Enum.map(&Enum.join([prefix, &1], "."))
   end
-
-  defp any_listeners?(listeners, full_channel) do
-    Enum.any?(listeners, fn {_pid, channels} -> full_channel in channels end)
-  end
-
-  defp in_scope?(%{"ident" => "any"}, _conf), do: true
-  defp in_scope?(%{"ident" => ident}, conf), do: Config.match_ident?(conf, ident)
-  defp in_scope?(_decoded, _conf), do: true
-
-  defp connect_and_listen(%State{conf: conf, conn: nil} = state) do
-    case Notifications.start_link(Repo.config(conf)) do
-      {:ok, conn} ->
-        for {_, full} <- @mappings,
-            do: Notifications.listen(conn, "#{conf.prefix}.#{full}")
-
-        %{state | conn: conn}
-
-      {:error, error} ->
-        trip_circuit(error, [], state)
-    end
-  end
-
-  defp connect_and_listen(state), do: state
 end
