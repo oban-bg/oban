3e704116ffb567e267e7861a32374be096aa0087
Parker Selbert <parker@sorentwo.com>
2021-12-27 16:31:15 -0600

    Point to github branch for postgrex changes

--- mix.exs
+++ mix.exs
@@ -162,7 +162,8 @@ defmodule Oban.MixProject do
     [
       {:ecto_sql, "~> 3.6"},
       {:jason, "~> 1.1"},
-      {:postgrex, "~> 0.14", path: "../postgrex", override: true},
+      {:postgrex, "~> 0.14",
+       github: "sorentwo/postgrex", branch: "notification-callback-module", override: true},
       {:telemetry, "~> 0.4 or ~> 1.0"},
       {:stream_data, "~> 0.4", only: [:test, :dev]},
       {:tzdata, "~> 1.0", only: [:test, :dev]},
