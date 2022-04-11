65f5a443c39becf71084869a279cccbbc93aa464
Parker Selbert <parker@sorentwo.com>
2022-04-11 13:14:34 -0500

    Introduce inline testing mode
    Inline testing immediately executes jobs as they are inserted, within
    the current process, and without ever touching the database. It's an
    alternative to the standard insert/verify/execute flow for testing.

---
+++ lib/oban/queue/inline_engine.ex
@@ -0,0 +1,136 @@
+defmodule Oban.Queue.InlineEngine do
+  @moduledoc false
+
+  @behaviour Oban.Queue.Engine
+
+  import DateTime, only: [utc_now: 0]
+
+  alias Ecto.{Changeset, Multi}
+  alias Oban.{Config, Job}
+  alias Oban.Queue.{BasicEngine, Engine, Executor}
+
+  @impl Engine
+  defdelegate init(conf, opts), to: BasicEngine
+
+  @impl Engine
+  def put_meta(_conf, meta, key, value), do: Map.put(meta, key, value)
+
+  @impl Engine
+  def check_meta(_conf, meta, _running), do: meta
+
+  @impl Engine
+  def refresh(_conf, meta), do: meta
+
+  @impl Engine
+  def insert_job(%Config{} = conf, %Changeset{} = changeset) do
+    {:ok, execute_job(conf, changeset)}
+  end
+
+  @impl Engine
+  def insert_job(%Config{} = conf, %Multi{} = multi, name, fun) when is_function(fun, 1) do
+    Multi.run(multi, name, fn repo, changes ->
+      {:ok, execute_job(%{conf | repo: repo}, fun.(changes))}
+    end)
+  end
+
+  @impl Engine
+  def insert_job(%Config{} = conf, %Multi{} = multi, name, %Changeset{} = changeset) do
+    Multi.run(multi, name, fn repo, _changes ->
+      {:ok, execute_job(%{conf | repo: repo}, changeset)}
+    end)
+  end
+
+  @impl Engine
+  def insert_all_jobs(%Config{} = conf, changesets) do
+    changesets
+    |> expand()
+    |> Enum.map(&execute_job(conf, &1))
+  end
+
+  @impl Engine
+  def insert_all_jobs(%Config{} = conf, %Multi{} = multi, name, wrapper) do
+    Multi.run(multi, name, fn repo, changes ->
+      conf = %{conf | repo: repo}
+
+      jobs =
+        wrapper
+        |> expand(changes)
+        |> Enum.map(&execute_job(conf, &1))
+
+      {:ok, jobs}
+    end)
+  end
+
+  @impl Engine
+  def fetch_jobs(_conf, meta, _running), do: {:ok, {meta, []}}
+
+  @impl Engine
+  def complete_job(_conf, _job), do: :ok
+
+  @impl Engine
+  def discard_job(_conf, _job), do: :ok
+
+  @impl Engine
+  def error_job(_conf, _job, seconds) when is_integer(seconds), do: :ok
+
+  @impl Engine
+  def snooze_job(_conf, _job, seconds) when is_integer(seconds), do: :ok
+
+  @impl Engine
+  def cancel_job(_conf, _job), do: :ok
+
+  @impl Engine
+  def cancel_all_jobs(_conf, _queryable), do: {:ok, {0, []}}
+
+  @impl Engine
+  def retry_job(_conf, _job), do: :ok
+
+  @impl Engine
+  def retry_all_jobs(_conf, _queryable), do: {:ok, 0}
+
+  # Changeset Helpers
+
+  defp expand(value), do: expand(value, %{})
+  defp expand(fun, changes) when is_function(fun, 1), do: expand(fun.(changes), changes)
+  defp expand(%{changesets: changesets}, _), do: expand(changesets, %{})
+  defp expand(changesets, _) when is_list(changesets), do: changesets
+
+  # Execution Helpers
+
+  defp execute_job(conf, changeset) do
+    changeset =
+      changeset
+      |> Changeset.put_change(:attempted_by, [conf.node])
+      |> Changeset.put_change(:attempted_at, utc_now())
+      |> Changeset.put_change(:scheduled_at, utc_now())
+      |> Changeset.update_change(:args, &json_encode_decode/1)
+      |> Changeset.update_change(:meta, &json_encode_decode/1)
+
+    case Changeset.apply_action(changeset, :insert) do
+      {:ok, job} ->
+        conf
+        |> Executor.new(job)
+        |> Executor.put(:safe, false)
+        |> Executor.call()
+        |> tap(&IO.inspect(&1.state))
+        |> complete_job()
+
+      {:error, changeset} ->
+        raise Ecto.InvalidChangesetError, action: :insert, changeset: changeset
+    end
+  end
+
+  defp json_encode_decode(map) do
+    map
+    |> Jason.encode!()
+    |> Jason.decode!()
+  end
+
+  defp complete_job(%{job: job, state: :success}) do
+    %Job{job | state: "completed", completed_at: utc_now()}
+  end
+
+  defp complete_job(%{job: job, result: {:snooze, snooze}, state: :snoozed}) do
+    %Job{job | state: "scheduled", scheduled_at: DateTime.add(utc_now(), snooze, :second)}
+  end
+end
