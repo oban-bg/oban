1d0932f0d44d6ba9cd41a9d7e36f300b75f804f5
Parker Selbert <parker@sorentwo.com>
2022-09-12 09:49:26 -0500

    Clean up naming in base oban_test

--- test/oban_test.exs
+++ test/oban_test.exs
@@ -1,45 +1,45 @@
 defmodule ObanTest do
-  use ExUnit.Case, async: true
+  use Oban.Case, async: true
 
-  describe "name" do
-    test "can be an arbitrary term" do
+  describe "start_link/1" do
+    test "name can be an arbitrary term" do
       assert {:ok, _} = start_supervised({Oban, name: make_ref(), repo: Oban.Test.Repo})
     end
 
-    test "is by default `Oban`" do
+    test "name defaults to `Oban`" do
       assert {:ok, pid} = start_supervised({Oban, repo: Oban.Test.Repo})
       assert Oban.whereis(Oban) == pid
     end
 
-    test "must be unique" do
+    test "name must be unique" do
       name = make_ref()
-      {:ok, pid} = start_supervised({Oban, name: name, repo: Oban.Test.Repo})
+      opts = [name: name, repo: Oban.Test.Repo]
 
-      assert Oban.start_link(name: name, repo: Oban.Test.Repo) ==
-               {:error, {:already_started, pid}}
+      {:ok, pid} = Oban.start_link(opts)
+      {:error, {:already_started, ^pid}} = Oban.start_link(opts)
     end
 
-    test "is used as a default child id" do
+    test "name is used as a default child id" do
       assert Supervisor.child_spec(Oban, []).id == Oban
       assert Supervisor.child_spec({Oban, name: :foo}, []).id == :foo
     end
   end
 
   describe "whereis/1" do
-    test "returns pid of the oban root process" do
-      name1 = make_ref()
-      {:ok, oban1} = start_supervised({Oban, name: name1, repo: Oban.Test.Repo})
+    test "returning the Oban root process's pid" do
+      name_1 = make_ref()
+      name_2 = make_ref()
 
-      name2 = make_ref()
-      {:ok, oban2} = start_supervised({Oban, name: name2, repo: Oban.Test.Repo})
+      {:ok, oban_1} = start_supervised({Oban, name: name_1, repo: Repo})
+      {:ok, oban_2} = start_supervised({Oban, name: name_2, repo: Repo})
 
-      refute oban1 == oban2
-      assert Oban.whereis(name1) == oban1
-      assert Oban.whereis(name2) == oban2
+      refute oban_1 == oban_2
+      assert Oban.whereis(name_1) == oban_1
+      assert Oban.whereis(name_2) == oban_2
     end
 
-    test "returns nil if root process not found" do
-      assert is_nil(Oban.whereis(make_ref()))
+    test "returning nil if root process not found" do
+      refute Oban.whereis(make_ref())
     end
   end
 end
