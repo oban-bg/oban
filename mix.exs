849a5a006566b6384e047ce4d66be3f031dfcc11
Parker Selbert <parker@sorentwo.com>
2022-12-23 12:12:48 -0600

    Make postgrex dependency optional

--- mix.exs
+++ mix.exs
@@ -150,7 +150,7 @@ defmodule Oban.MixProject do
       {:ecto_sql, "~> 3.6"},
       {:ecto_sqlite3, "~> 0.9", optional: true},
       {:jason, "~> 1.1"},
-      {:postgrex, "~> 0.16"},
+      {:postgrex, "~> 0.16", optional: true},
       {:telemetry, "~> 0.4 or ~> 1.0"},
       {:stream_data, "~> 0.4", only: [:test, :dev]},
       {:tz, "~> 0.24", only: [:test, :dev]},
