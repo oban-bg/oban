85e16f7b25e3b35576dc049a9f6cefc8a84ed91f
Yordis Prieto <yordis.prieto@gmail.com>
2023-08-25 09:29:57 -0400

    Add oban instance module (#914)
    Add oban instance module to automate creating facades with config

--- lib/oban.ex
+++ lib/oban.ex
@@ -14,6 +14,10 @@ defmodule Oban do
   alias Oban.{Config, Engine, Job, Midwife, Notifier, Peer, Registry, Stager}
   alias Oban.Queue.{Drainer, Producer}
 
+  @typedoc """
+  The name of an Oban instance. This is used to identify instances in the internal registry for
+  configuration lookup.
+  """
   @type name :: term()
 
   @type queue_name :: atom() | binary()
@@ -1116,6 +1120,130 @@ defmodule Oban do
 
     if local_only && is_nil(Registry.whereis(name, {:producer, to_string(queue)})) do
       raise ArgumentError, "queue #{inspect(queue)} does not exist locally"
+    end
+  end
+
+  @doc """
+  Creates a facade for `Oban` functions and automates fetching configuration from the application
+  environment.
+
+  It allows you to avoid having to pass the `t:Oban.name/0` value to the `Oban` functions, as it is automatically set to
+  the module name. As well as allowing you to configure the `Oban` instance in the application's configuration using
+  the module name under a given OTP application key.
+
+  ## Examples
+
+  ### In an application module
+
+      defmodule MyApp.Oban do
+        use Oban.Instance,
+          otp_app: :my_app,
+          repo: MyApp.Repo
+      end
+
+  Now you can register the `MyApp.Oban` module in the application's supervision tree:
+
+      defmodule MyApp.Application do
+        use Application
+
+        def start(_type, _args) do
+          children = [
+            MyApp.Repo,
+            MyApp.Oban
+          ]
+
+          opts = [strategy: :one_for_one, name: MyApp.Supervisor]
+          Supervisor.start_link(children, opts)
+        end
+      end
+
+  ### Avoiding the need of passing the `Oban` instance
+
+  Instead of calling `Oban` functions passing the `Oban` instance, you can use the `Oban.Instance` module directly:
+
+      Oban.Instance.insert(MyApp.MyJob.new(args: %{ "field" => "value" }))
+  """
+  defmacro __using__(opts \\ []) do
+    {opts, child_opts} = Keyword.split(opts, [:otp_app])
+    otp_app = Keyword.fetch!(opts, :otp_app)
+
+    # credo:disable-for-next-line Credo.Check.Refactor.LongQuoteBlocks
+    quote do
+      def child_spec(opts) do
+        unquote(child_opts)
+        |> Keyword.merge(Application.get_env(unquote(otp_app), __MODULE__, []))
+        |> Keyword.merge(opts)
+        |> Keyword.put(:name, __MODULE__)
+        |> Oban.child_spec()
+      end
+
+      def cancel_all_jobs(queryable) do
+        Oban.cancel_all_jobs(__MODULE__, queryable)
+      end
+
+      def cancel_job(job_or_id) do
+        Oban.cancel_job(__MODULE__, job_or_id)
+      end
+
+      def check_queue(opts) do
+        Oban.check_queue(__MODULE__, opts)
+      end
+
+      def config do
+        Oban.config(__MODULE__)
+      end
+
+      def drain_queue(opts) do
+        Oban.drain_queue(__MODULE__, opts)
+      end
+
+      def insert(changeset, opts \\ []) do
+        Oban.insert(__MODULE__, changeset, opts)
+      end
+
+      def insert(multi, multi_name, changeset, opts \\ []) do
+        Oban.insert(__MODULE__, multi, multi_name, changeset, opts)
+      end
+
+      def insert!(changeset, opts \\ []) do
+        Oban.insert!(__MODULE__, changeset, opts)
+      end
+
+      def insert_all(changesets, opts) do
+        Oban.insert_all(__MODULE__, changesets, opts)
+      end
+
+      def insert_all(multi, multi_name, changesets, opts) do
+        Oban.insert_all(__MODULE__, multi, multi_name, changesets, opts)
+      end
+
+      def start_queue(opts) do
+        Oban.start_queue(__MODULE__, opts)
+      end
+
+      def pause_queue(opts) do
+        Oban.pause_queue(__MODULE__, opts)
+      end
+
+      def resume_queue(opts) do
+        Oban.resume_queue(__MODULE__, opts)
+      end
+
+      def scale_queue(opts) do
+        Oban.scale_queue(__MODULE__, opts)
+      end
+
+      def stop_queue(opts) do
+        Oban.stop_queue(__MODULE__, opts)
+      end
+
+      def retry_job(job_or_id) do
+        Oban.retry_job(__MODULE__, job_or_id)
+      end
+
+      def retry_all_jobs(queryable) do
+        Oban.retry_all_jobs(__MODULE__, queryable)
+      end
     end
   end
 end
