b0529681871bdc99bdf5a248043aaab061d87a2a
Parker Selbert <parker@sorentwo.com>
2023-10-12 13:43:01 -0500

    Stop stager telemetry test from flaking

--- test/oban/stager_test.exs
+++ test/oban/stager_test.exs
@@ -6,27 +6,27 @@ defmodule Oban.StagerTest do
 
   describe "integration" do
     test "descheduling jobs to make them available for execution" do
-      TelemetryHandler.attach_events()
-
-      job_1 = insert!(%{}, state: "scheduled", scheduled_at: seconds_ago(1))
-      job_2 = insert!(%{}, state: "scheduled", scheduled_at: seconds_from_now(1))
+      job_1 = insert!(%{}, state: "scheduled", scheduled_at: seconds_ago(10))
+      job_2 = insert!(%{}, state: "scheduled", scheduled_at: seconds_from_now(10))
       job_3 = insert!(%{}, state: "retryable")
 
       start_supervised_oban!(stage_interval: 5, testing: :disabled)
 
-      assert_receive {:event, :start, %{system_time: _}, %{conf: _, plugin: Stager}}
+      with_backoff(fn ->
+        assert %{state: "available"} = Repo.reload(job_1)
+        assert %{state: "scheduled"} = Repo.reload(job_2)
+        assert %{state: "available"} = Repo.reload(job_3)
+      end)
+    end
+
+    test "emitting telemetry data for staged jobs" do
+      TelemetryHandler.attach_events()
+
+      start_supervised_oban!(stage_interval: 5, testing: :disabled)
+
       assert_receive {:event, :stop, %{duration: _}, %{plugin: Stager} = meta}
 
-      assert %{staged_count: 2, staged_jobs: jobs} = meta
-
-      assert ~w(retryable scheduled) =
-               jobs
-               |> Enum.map(& &1.state)
-               |> Enum.sort()
-
-      assert %{state: "available"} = Repo.reload(job_1)
-      assert %{state: "scheduled"} = Repo.reload(job_2)
-      assert %{state: "available"} = Repo.reload(job_3)
+      assert %{staged_count: _, staged_jobs: []} = meta
     end
 
     test "switching to local mode without functional pubsub" do
