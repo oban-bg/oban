eb605c87812ea1628f37d7fb293e753e5a26301c
Jon Rowe <hello@jonrowe.co.uk>
2023-10-21 21:25:03 +0200

    Allow unique's period option to take units. (#975)
    Other parts of Oban (e.g. rate-limiting) can take a period in the form `{n, unit}`, which is missing from the unique `:period` option.

--- lib/oban/job.ex
+++ lib/oban/job.ex
@@ -221,7 +221,7 @@ defmodule Oban.Job do
 
   Insert a job, ensuring that it is unique within the past minute:
 
-      MyApp.Worker.new(%{id: 1}, unique: [period: 60])
+      MyApp.Worker.new(%{id: 1}, unique: [period: {1, :minute}])
 
   Insert a unique job where the period is compared to the `scheduled_at` timestamp rather than
   `inserted_at`:
@@ -367,11 +367,34 @@ defmodule Oban.Job do
   end
 
   @doc false
+  @spec cast_period(pos_integer() | {atom(), pos_integer()}) :: pos_integer()
+  def cast_period({value, unit}) do
+    unit = to_string(unit)
+
+    cond do
+      unit in ~w(second seconds) -> value
+      unit in ~w(minute minutes) -> value * 60
+      unit in ~w(hour hours) -> value * 60 * 60
+      unit in ~w(day days) -> value * 24 * 60 * 60
+      true -> unit
+    end
+  end
+
+  def cast_period(period), do: period
+
+  @valid_period_units ~w(second seconds minute minutes hour hours day days)a
+
+  @doc false
   @spec valid_unique_opt?({:fields | :period | :states, [atom()] | integer()}) :: boolean()
   def valid_unique_opt?({:fields, [_ | _] = fields}), do: fields -- [:meta | @unique_fields] == []
   def valid_unique_opt?({:keys, []}), do: true
   def valid_unique_opt?({:keys, [_ | _] = keys}), do: Enum.all?(keys, &is_atom/1)
   def valid_unique_opt?({:period, :infinity}), do: true
+
+  def valid_unique_opt?({:period, {period, unit}}) do
+    is_integer(period) and period > 0 and unit in @valid_period_units
+  end
+
   def valid_unique_opt?({:period, period}), do: is_integer(period) and period > 0
   def valid_unique_opt?({:states, [_ | _] = states}), do: states -- states() == []
   def valid_unique_opt?({:timestamp, stamp}), do: stamp in ~w(inserted_at scheduled_at)a
@@ -447,7 +470,7 @@ defmodule Oban.Job do
           |> Map.new()
           |> Map.put_new(:fields, @unique_fields)
           |> Map.put_new(:keys, [])
-          |> Map.put_new(:period, @unique_period)
+          |> Map.update(:period, @unique_period, &cast_period/1)
           |> Map.put_new(:states, @unique_states)
           |> Map.put_new(:timestamp, @unique_timestamp)
 
