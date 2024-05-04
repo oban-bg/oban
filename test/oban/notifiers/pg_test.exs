1af37478c5c0ad2508d207bb107b13e609d0bf1b
Parker Selbert <parker@sorentwo.com>
2024-05-04 15:51:06 +0100

    Monitor pg scope before making test assertions

--- test/oban/notifiers/pg_test.exs
+++ test/oban/notifiers/pg_test.exs
@@ -4,10 +4,20 @@ defmodule Oban.Notifiers.PGTest do
   alias Oban.Notifier
   alias Oban.Notifiers.PG
 
+  setup do
+    :pg.start_link(Oban.Notifiers.PG)
+    :pg.monitor_scope(Oban.Notifiers.PG)
+
+    :ok
+  end
+
   describe "namespacing" do
     test "namespacing by configured prefix without an override" do
       name_1 = start_supervised_oban!(notifier: PG, prefix: "pg_test")
       name_2 = start_supervised_oban!(notifier: PG, prefix: "pg_test")
+
+      assert_joined("pg_test")
+      assert_joined("pg_test")
 
       :ok = Notifier.listen(name_1, :signal)
       :ok = Notifier.notify(name_2, :signal, %{incoming: "message"})
@@ -19,10 +29,17 @@ defmodule Oban.Notifiers.PGTest do
       name_1 = start_supervised_oban!(notifier: {PG, namespace: :pg_test}, prefix: "pg_a")
       name_2 = start_supervised_oban!(notifier: {PG, namespace: :pg_test}, prefix: "pg_b")
 
+      assert_joined(:pg_test)
+      assert_joined(:pg_test)
+
       :ok = Notifier.listen(name_1, :signal)
       :ok = Notifier.notify(name_2, :signal, %{incoming: "message"})
 
       assert_receive {:notification, :signal, %{"incoming" => "message"}}
     end
+  end
+
+  defp assert_joined(group) do
+      assert_receive {_ref, :join, ^group, _pids}
   end
 end
