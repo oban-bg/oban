2024965fa30e123b2e0428a2f6a71e4f477f0012
Parker Selbert <parker@sorentwo.com>
2024-06-21 13:03:15 -0500

    Raise minimum tested Elixir version to 1.14

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Oban.MixProject do
     [
       app: :oban,
       version: @version,
-      elixir: "~> 1.13",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
