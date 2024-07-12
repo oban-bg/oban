4d43326b8f42b36f536011a6204f9ae53748ec67
Parker Selbert <parker@sorentwo.com>
2024-07-12 09:41:23 -0500

    Merge oban_jobs table migrations

--- test/support/postgres/migrations/20190210214150_add_oban_jobs_table.exs
+++ test/support/postgres/migrations/20190210214150_add_oban_jobs_table.exs
@@ -1,9 +1,13 @@
 defmodule Oban.Test.Repo.Postgres.Migrations.AddObanJobsTable do
   use Ecto.Migration
 
-  defdelegate up, to: Oban.Migrations
+  def up do
+    Oban.Migrations.up(unlogged: false)
+    Oban.Migrations.up(prefix: "private", unlogged: false)
+  end
 
   def down do
+    Oban.Migrations.down(prefix: "private", version: 1)
     Oban.Migrations.down(version: 1)
   end
 end
