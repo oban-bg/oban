c4cb95c12bbaec63a610ac5dee9f57c0b29b9f93
Mikael Gråborg <mikael.graborg@kivra.com>
2025-03-28 11:08:03 +0100

    fix: use correct migration syntax

--- lib/oban/migrations/postgres/v13.ex
+++ lib/oban/migrations/postgres/v13.ex
@@ -3,8 +3,13 @@ defmodule Oban.Migrations.Postgres.V13 do
 
   use Ecto.Migration
 
-  def change do
-    create index(:oban_jobs, [:state, :cancelled_at])
-    create index(:oban_jobs, [:state, :discarded_at])
+  def up(%{prefix: prefix}) do
+    create_if_not_exists index(:oban_jobs, [:state, :cancelled_at], prefix: prefix)
+    create_if_not_exists index(:oban_jobs, [:state, :discarded_at], prefix: prefix)
+  end
+
+  def down(%{prefix: prefix}) do
+    drop_if_exists index(:oban_jobs, [:state, :discarded_at], prefix: prefix)
+    drop_if_exists index(:oban_jobs, [:state, :cancelled_at], prefix: prefix)
   end
 end
