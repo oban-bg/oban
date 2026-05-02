bd4e1f256b9e9278852ae13cc73901d667b75bc3
Parker Selbert <parker@sorentwo.com>
2026-05-02 11:25:31 +0100

    Scope receives for notifier test assertions
    Concurrent tests using the same PG notifier prefix and channel could
    interfere and cause flickering test failures.

--- test/oban/notifier_test.exs
+++ test/oban/notifier_test.exs
@@ -40,7 +40,7 @@ for notifier <- [Oban.Notifiers.Isolated, Oban.Notifiers.PG, Oban.Notifiers.Post
             tuple: {1, :second}
           })
 
-          assert_receive {:notification, :signal, notice}
+          assert_receive {:notification, :signal, %{"date" => _} = notice}
           assert %{"date" => "2021-08-09", "keyword" => [["a", 1], ["b", 1]]} = notice
           assert %{"map" => %{"tuple" => [1, "second"]}, "tuple" => [1, "second"]} = notice
         end)
@@ -79,8 +79,8 @@ for notifier <- [Oban.Notifiers.Isolated, Oban.Notifiers.PG, Oban.Notifiers.Post
           :ok = Notifier.notify(name, :gossip, %{foo: "bar", ident: ident})
           :ok = Notifier.notify(name, :signal, %{foo: "baz", ident: "bogus.ident"})
 
-          assert_receive {:notification, :gossip, _}
-          refute_received {:notification, :signal, _}
+          assert_receive {:notification, :gossip, %{"foo" => "bar"}}
+          refute_received {:notification, :signal, %{"foo" => "baz"}}
         end)
       end
     end
